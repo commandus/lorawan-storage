8ef99ba334d5ba50724e878eab6a7b996706b163
andrei <andrei.i.ivanov@gmail.com>
2024-05-19 16:34:14 +0900

    Start adding URN serialization for LoRaWAN Device Identification QR Codes

--- lorawan/storage/serialization/urn-helper.cpp
+++ lorawan/storage/serialization/urn-helper.cpp
@@ -9,11 +9,146 @@ const char* SCHEMA_ID = "D0:";
 const char* DEF_PROFILE_ID = "FFFFFFFF";
 const char* DLMT = ":";
 
+static const char FLD_PREFIXES[17] = {
+    // mandatory in order
+    '0',    // appEui
+    '1',    // devEui
+    '2',    // profile id (4 bytes)
+    // optional with prefix tag
+    'C',    // check sum
+    'O',    // owner token;
+    'S',    // serial number
+    // proprietary with prefix tag 'P'
+    'D',    // device address
+    'T',    // activation
+    'C',     // device class
+    'W',     // nwkSKey
+    'S',    // appSKey
+    'V',    // LoRaWAN version
+    'A',    // appKey
+    'N',    // nwkKey
+    'O',    // devNonce
+    'J',    // joinNonce
+    'X'     // 'command': 'A', 'I', 'L', 'C', 'N', 'P', 'R', 'S', 'E'
+};
+
 static uint16_t calcCheckSum(
     const std::string &urn
 )
 {
     return crc16modbus((uint8_t *) urn.c_str(), urn.size());
+}
+
+LorawanIdentificationURN::LorawanIdentificationURN()
+    : crc(0xffff), command('\0')
+{
+
+}
+
+LorawanIdentificationURN::LorawanIdentificationURN(
+    const std::string &urn
+)
+{
+    parse(urn);
+}
+
+bool LorawanIdentificationURN::parse(
+    const std::string &urn
+)
+{
+    bool r = true;
+    size_t p = 0;
+    int count = 0;
+    while ((p = urn.find(':', p)) != std::string::npos) {
+        std::string token = urn.substr(0, p);
+        switch (count) {
+            case 0:
+                if (token != "LW")
+                    return false;
+                break;
+            case 1:
+                if (token != "D0")
+                    return false;
+                break;
+            case 2:
+                string2DEVEUI(networkIdentity.devid.appEUI, token);
+                break;
+            case 3:
+                string2DEVEUI(networkIdentity.devid.devEUI, token);
+                break;
+            default:
+                // optional
+            {
+                if (token.empty())
+                    continue;
+                switch (token[0]) {
+                    case 'C':
+                        crc = (uint16_t) strtoul(token.c_str() + 1, nullptr, 16);
+                        break;
+                    case 'O':
+                        ownerToken = token.substr(1);
+                    case 'S':
+                        serialNumber = token.substr(1);
+                        break;
+                    case 'P':
+                        if (token.size() < 2)
+                            continue;
+                        switch (token[1]) {
+                            case 'D':    // device address
+                                string2DEVADDR(networkIdentity.devaddr, token.substr(2));
+                                break;
+                            case 'T':    // activation
+                                networkIdentity.devid.activation = string2activation(token.substr(2));
+                                break;
+                            case 'C':     // device class
+                                networkIdentity.devid.setClass(string2deviceclass(token.substr(2)));
+                                break;
+                            case 'W':     // nwkSKey
+                                string2KEY(networkIdentity.devid.nwkSKey, token.substr(2));
+                                break;
+                            case 'S':    // appSKey
+                                string2KEY(networkIdentity.devid.appSKey, token.substr(2));
+                                break;
+                            case 'V':    // LoRaWAN version
+                                networkIdentity.devid.version = string2LORAWAN_VERSION(token.substr(2));
+                                break;
+                            case 'A':    // appKey
+                                string2KEY(networkIdentity.devid.appKey, token.substr(2));
+                                break;
+                            case 'N':    // nwkKey
+                                string2KEY(networkIdentity.devid.nwkKey, token.substr(2));
+                                break;
+                            case 'O':    // devNonce
+                                networkIdentity.devid.devNonce = string2DEVNONCE(token.substr(2));
+                                break;
+                            case 'J':    // joinNonce
+                                string2JOINNONCE(networkIdentity.devid.joinNonce, token.substr(2));
+                                break;
+                            case 'X':     // 'command': 'A', 'I', 'L', 'C', 'N', 'P', 'R', 'S', 'E'
+                                if (token.size() > 2)
+                                    command = token[3];
+                                else
+                                    token = '\0';
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+        p++;
+        count++;
+    }
+    LW:D0:
+    return r;
+}
+
+std::string LorawanIdentificationURN::toString() const
+{
+    return NETWORKIDENTITY2URN(networkIdentity, ownerToken, serialNumber, true, false, nullptr);
 }
 
 std::string mkURN(
@@ -73,7 +208,7 @@ std::string NETWORKIDENTITY2URN(
         proprietary.push_back("V" + LORAWAN_VERSION2string(networkIdentity.devid.version));
         proprietary.push_back("A" + KEY2string(networkIdentity.devid.appKey));
         proprietary.push_back("N" + KEY2string(networkIdentity.devid.nwkKey));
-        proprietary.push_back("D" + DEVNONCE2string(networkIdentity.devid.devNonce));
+        proprietary.push_back("O" + DEVNONCE2string(networkIdentity.devid.devNonce));
         proprietary.push_back("J" + JOINNONCE2string(networkIdentity.devid.joinNonce));
     }
 
--- lorawan/storage/serialization/urn-helper.h
+++ lorawan/storage/serialization/urn-helper.h
@@ -5,8 +5,37 @@
  * TR005 LoRaWAN Device Identification QR Codes
  * @see https://resources.lora-alliance.org/document/tr005-lorawan-device-identification-qr-codes
  */
+#include <string>
 #include <vector>
 #include "lorawan/lorawan-types.h"
+
+typedef enum ProprietaryXCommand {
+    PROPRIETARY_X_COMMAND_NONE = '\0',
+    PROPRIETARY_X_COMMAND_IDENTITY_ADDR = 'A',
+    PROPRIETARY_X_COMMAND_IDENTITY_EUI = 'I',
+    PROPRIETARY_X_COMMAND_IDENTITY_LIST = 'L',
+    PROPRIETARY_X_COMMAND_IDENTITY_COUNT = 'C',
+    PROPRIETARY_X_COMMAND_IDENTITY_NEXT = 'N',
+    PROPRIETARY_X_COMMAND_IDENTITY_ASSIGN = 'P',
+    PROPRIETARY_X_COMMAND_IDENTITY_RM = 'R',
+    PROPRIETARY_X_COMMAND_IDENTITY_FORCE_SAVE = 'S',
+    PROPRIETARY_X_COMMAND_IDENTITY_CLOSE_RESOURCES = 'E'
+} ProprietaryXCommand;
+
+class LorawanIdentificationURN {
+protected:
+    bool parse(const std::string &urn);
+public:
+    NETWORKIDENTITY networkIdentity;
+    PROFILEID profileId;
+    uint16_t crc;
+    std::string ownerToken;
+    std::string serialNumber;
+    char command;
+    LorawanIdentificationURN();
+    LorawanIdentificationURN(const std::string &urn);
+    std::string toString() const;
+};
 
 std::string mkURN(
     const DEVEUI &appEui,
