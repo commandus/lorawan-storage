7f8454cd0e2b48bc14d7494946dff26e82fc9868
andrei <andrei.i.ivanov@gmail.com>
2025-01-22 12:46:19 +0900

    Some v

--- tests/test-heatshrink.cpp
+++ tests/test-heatshrink.cpp
@@ -17,20 +17,52 @@ static size_t testEncode(
 ) {
     heatshrink_encoder hse;
     heatshrink_encoder_reset(&hse);
-
     size_t count = 0;
-    uint32_t sunk = 0;
+    size_t sunk = 0;
     size_t polled = 0;
     while (sunk < inputSize) {
+        heatshrink_encoder_sink(&hse, (uint8_t *) &input[sunk], inputSize - sunk, &count);
         sunk += count;
+        if (sunk == inputSize)
+            heatshrink_encoder_finish(&hse);
         HSE_poll_res pres;
         do {
             pres = heatshrink_encoder_poll(&hse, (uint8_t *) output + polled, outputSize - polled, &count);
             polled += count;
         } while (pres == HSER_POLL_MORE);
-        if (polled >= outputSize) {
+        if (sunk == inputSize)
+            heatshrink_encoder_finish(&hse);
+        if (polled >= outputSize)
             return 0;
-        }
+    }
+    return polled;
+}
+
+static size_t testDecode(
+    char *output,
+    size_t outputSize,
+    const char *input,
+    size_t inputSize
+) {
+    heatshrink_decoder hsd;
+    heatshrink_decoder_reset(&hsd);
+    size_t count = 0;
+    size_t sunk = 0;
+    size_t polled = 0;
+    while (sunk < inputSize) {
+        heatshrink_decoder_sink(&hsd, (uint8_t *) &input + sunk, inputSize - sunk, &count);
+        sunk += count;
+        if (sunk == inputSize)
+            heatshrink_decoder_finish(&hsd);
+        HSD_poll_res pres;
+        do {
+            pres = heatshrink_decoder_poll(&hsd, (uint8_t *) output + polled, outputSize - polled, &count);
+            polled += count;
+        } while (pres == HSDR_POLL_MORE);
+        if (sunk == inputSize)
+            heatshrink_decoder_finish(&hsd);
+        if (polled > outputSize)
+            return 0;
     }
     return polled;
 }
@@ -57,9 +89,17 @@ int main() {
     char *compressedBuffer = (char *) malloc(compressedBufferSize);
     memset(compressedBuffer, 0, compressedBufferSize);
     size_t compressedSize = testEncode(compressedBuffer, compressedBufferSize, s.c_str(), s.size());
-    free(compressedBuffer);
+
 
     std::cout << s.size() << "->" << compressedSize << std::endl;
-    // testDecode();
+
+    size_t decompressedBufferSize = s.size();
+    char *decompressedBuffer = (char *) malloc(decompressedBufferSize);
+    memset(decompressedBuffer, 0, decompressedBufferSize);
+    size_t decompressedSize = testDecode(decompressedBuffer, decompressedBufferSize, compressedBuffer, compressedSize);
+    std::cout << compressedSize << "->" << decompressedSize << "(" << decompressedBufferSize << ")" << std::endl;
+
+    free(decompressedBuffer);
+    free(compressedBuffer);
     return 0;
 }
